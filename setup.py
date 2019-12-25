5d419f411174bb8e0dec60f5c6c604e900cec0cd
Alex Makasoff <amakasoff@arlo.com>
2019-12-24 19:46:20 -0800

    added .ini files to tests

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name="rtframework",
-    version="0.0.7",
+    version="0.0.8",
     author="Alex Makasoff",
     author_email="alexmakasoff@gmail.com",
     description="Test framework geared for stress and load testing",
--- tests/test_filereader.py
+++ tests/test_filereader.py
@@ -8,3 +8,11 @@ def test_read_text_file():
     test_file = cwd / "tests" / "mockfile.txt"
     decoded_file = read(test_file)
     assert(expected == decoded_file)
+
+
+def test_read_ini_file():
+    expected = "[test section]\nfoo = bar"
+    cwd = pathlib.Path.cwd()
+    test_file = cwd / "tests" / "mockfile.ini"
+    decoded_file = read(test_file)
+    assert(expected == decoded_file)
